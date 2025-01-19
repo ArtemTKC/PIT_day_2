60e57cd1a555ea465f1ce60aac0d9846e7594e86
Artem Tkachuk <tkachuk_art@kpnl145.kyiv.ua>
2025-01-19 10:50:42 +0200

    Update last_prime.cpp
    fixed prime

--- last_prime.cpp
+++ last_prime.cpp
@@ -12,6 +12,22 @@ void in_mas(int mas[], int N){
         cin >> mas[i];
     }
 }
+/*
+bool prime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}*/
 
 bool happy(int N)
 {
